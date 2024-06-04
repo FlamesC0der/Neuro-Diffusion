6b67ea5c490d809edc6ed458dff838fd21da14c1
Alexey <alexm20games@gmail.com>
2024-06-05 00:31:36 +0300

    upd start.bat & start.command

--- start.bat
+++ start.bat
@@ -2,5 +2,6 @@
 cd /d %~dp0
 call .venv\Scripts\activate
 pip install -r requirements.txt
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 cls
 python main.py
--- start.command
+++ start.command
@@ -2,7 +2,7 @@
 dir=$(dirname "$0")
 cd "$dir"
 source .venv/bin/activate
-
 pip install -r requirements.txt
+pip install torch torchvision torchaudio
 clear
 python3 main.py
